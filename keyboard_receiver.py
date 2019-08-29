04156f21f110fc9c14d9a177260a1d298931a05a
Kasimir Jyry Santeri Ilmonen <jyrysanteri@hotmail.com>
2019-08-29 12:08:17 +0300

    Asetettu viesti 'a' painamaan nappia 'a'.

--- keyboard_receiver.py
+++ keyboard_receiver.py
@@ -1,8 +1,22 @@
 from pynput.keyboard import Key, Controller
 import time
+import socket
+
+UDP_PORT = 5005
 
 keyboard = Controller()
 
-time.sleep(2)
-keyboard.press('a')
-keyboard.release('a')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', UDP_PORT));
+
+print("Aloitetaan kuuntelu")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print("received message:", data)
+    if data.decode(encoding="utf-8") == "a":
+        keyboard.press('a')
+
+#time.sleep(2)
+#keyboard.press('a')
+#keyboard.release('a')
